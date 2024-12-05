16e59b4d671289fb1b06b4b385221bffc294aa35
root <root@HP2020098.nlinks.local>
2024-12-05 19:06:13 +0900

    モデル作成、Seedeでテストデータ挿入、ブレードファイル作成

--- src/Laravel9TaskList/app/Http/Controllers/TaskController.php
+++ src/Laravel9TaskList/app/Http/Controllers/TaskController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
 
 class TaskController extends Controller
 {
 	// index メソッドを追加する
 	public function index() 
 	{
-	  return "Hello worldfdsafsafs";
+		$folders = Folder::all();
+		return view('tasks/index', [
+			'folders' => $folders,
+	]);
 	}
 }
--- src/Laravel9TaskList/database/seeders/FoldersTableSeeder.php
+++ src/Laravel9TaskList/database/seeders/FoldersTableSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// PHPのDateTimeクラスをオーバーラップした日付操作ライブラリ
 use Carbon\Carbon;
+// DBファサードインポート
 use Illuminate\Support\Facades\DB;
 
 class FoldersTableSeeder extends Seeder
@@ -17,6 +19,14 @@ class FoldersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $titles = ['プライベート', '仕事', '旅行'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
     }
 }
